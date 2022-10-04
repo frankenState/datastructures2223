84c9229e37faf2f1c7a6e3192a4245c3540f5f90
frankenState <ubayfranklou@gmail.com>
2022-10-04 10:28:27 +0800

    Saving last state.

--- src/M2L2/RecursionExamples.java
+++ src/M2L2/RecursionExamples.java
@@ -38,7 +38,8 @@ public class RecursionExamples {
 //        System.out.println("world => " + isPalindrome("world"));
 
     
-          printMe("Hi", 5);
+//          printMe("Hi", 5);
+          System.out.println("Sum => " + sumNums(5));
         
     }
     
@@ -48,6 +49,12 @@ public class RecursionExamples {
            System.out.println(times + ") " + str);
            printMe(str, times - 1);
         }
+    }
+    
+    public static int sumNums(int n){
+        if (n > 0)
+            return n + sumNums(n - 1);
+        return n;
     }
     
 }
